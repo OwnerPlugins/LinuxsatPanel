0a95d09badcb32ea9a52cd45572b1c5d4ce36465
pQu4k3r <75614741+pQu4k3r@users.noreply.github.com>
2026-09-14 21:04:50 +0000

    Bump version to 3.0.6

--- usr/lib/enigma2/python/Plugins/Extensions/LinuxsatPanel/__init__.py
+++ usr/lib/enigma2/python/Plugins/Extensions/LinuxsatPanel/__init__.py
@@ -35,7 +35,7 @@ __author__ = "Lululla"
 __email__ = "ekekaz@gmail.com"
 __copyright__ = 'Copyright (c) 2024 Lululla'
 __license__ = "GPL-v2"
-__version__ = "3.0.5"
+__version__ = "3.0.6"
 
 
 def check_and_install_requests():
